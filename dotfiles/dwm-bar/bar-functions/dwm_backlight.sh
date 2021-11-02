0bbe10effcd237447d3eef9339ad754abaea83be
Franklin David Macias Avellan <franklin.macias864@gmail.com>
2021-11-02 22:52:55 +0100

    ArchLinux configuration files

---
+++ dotfiles/dwm-bar/bar-functions/dwm_backlight.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# A dwm_bar module to display the current backlight brighness with xbacklight
+# Joe Standring <git@joestandring.com>
+# GNU GPLv3
+
+# Dependencies: xbacklight
+
+dwm_backlight () {
+    #printf "%s☀ %.0f%s\n" "$SEP1" "$(xbacklight)" "$SEP2"
+    printf "%s %.0f%s\n" "$SEP1" "$(enlighten)" "$SEP2"
+
+}
+
+dwm_backlight
